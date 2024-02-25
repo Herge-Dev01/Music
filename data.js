6eb9d0b5142072da3e25ecb0fe899e488e52cb7a
Niriantsoa <niriantsoaherge@gmail.com>
2024-02-25 20:13:15 +0300

    Initialize Music Audio player

---
+++ data.js
@@ -0,0 +1,33 @@
+let songs = [
+    {
+        name: "Joyeux_anniversaire",    
+        artist: "Mr Sayda",
+        cover:"img/mr sayda.jpg",
+        path:"audio/joyeux_anniversaire_misie_sayda_mp3_45760.mp3"
+    },
+
+    {
+        name: "Ampy anahy",    
+        artist: "Lion Hill",
+        cover:"img/Lion hill.jpg",
+        path:"audio/Lion Hill - Ampy anahy 2021-Official Video-.mp3"
+    },
+
+    {
+        name: "Donia",    
+        artist: "Lion Hill",
+        cover:"img/Lion hill.jpg",
+        path:"audio/lion_hill_donia_official_video_mp3_46967.mp3"
+    },
+
+    {
+        name: "Mila Olo",    
+        artist: "Lion Hill",
+        cover:"img/Lion hill.jpg",
+        path:"audio/lion_hill_mila_olo_visualizer_mp3_13913.mp3"
+    }
+
+    // Add as you want
+
+    
+]
